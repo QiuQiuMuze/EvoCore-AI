e03bd5cd5fc44610831474e6d5a14c087c439d9a
QiuQiuMuze <161425209+QiuQiuMuze@users.noreply.github.com>
2025-10-01 14:22:53 -0400

    Ensure clone supplies subclass constructor requirements

---
+++ core/units/__init__.py
@@ -0,0 +1,24 @@
+from .cog_unit import CogUnit, EmitterUnit, ProcessorUnit, SensorUnit
+from .constants import (
+    ENABLE_MINI_LEARN,
+    FOLLOW_INPUT_DEVICE,
+    MAX_OUTPUT_DIM,
+    ROLE_SPLIT_RULE,
+    SPLIT_HI_ES_TABLE,
+    SPLIT_HI_P_TABLE,
+    TOL_FRAC_SPLIT,
+)
+
+__all__ = [
+    "CogUnit",
+    "SensorUnit",
+    "ProcessorUnit",
+    "EmitterUnit",
+    "ENABLE_MINI_LEARN",
+    "FOLLOW_INPUT_DEVICE",
+    "MAX_OUTPUT_DIM",
+    "ROLE_SPLIT_RULE",
+    "SPLIT_HI_ES_TABLE",
+    "SPLIT_HI_P_TABLE",
+    "TOL_FRAC_SPLIT",
+]
